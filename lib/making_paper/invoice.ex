93df0d21dde6e2e2ffc06c294f7e39fc529a58af
Shawn O'Neill <shawn@sanswork.com>
2017-11-09 21:57:13 +1100

    added invoice retrieval

--- lib/making_paper/invoice.ex
+++ lib/making_paper/invoice.ex
@@ -10,6 +10,19 @@ defmodule MakingPaper.Invoice do
     {upcoming, full_list}
   end
 
+  def get(customer_id, invoice_id) do
+    case Stripe.Invoice.retrieve(invoice_id) do
+      {:ok, invoice} ->
+        if invoice.customer == customer_id do
+          {:ok, invoice}
+        else
+          {:error, :not_found}
+        end
+      error ->
+        error
+    end
+  end
+
   def add_item(customer_id, amount, currency, description, discountable=true) do
     Stripe.Request.create("invoiceitems", %{customer: customer_id, amount: amount, currency: currency, description: description, discountable: discountable},%{customer: [:create], amount: [:create], currency: [:create], description: [:create], discountable: [:create]}, [])
   end
