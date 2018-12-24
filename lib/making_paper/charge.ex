ca1185e2565e4adcc5ca31b4c6d5ffaba6e4d122
Shawn O'Neill <shawn@sanswork.com>
2018-12-25 10:43:57 +1100

    added auth/capture and refunds to charges

--- lib/making_paper/charge.ex
+++ lib/making_paper/charge.ex
@@ -2,4 +2,16 @@ defmodule MakingPaper.Charge do
   def create(customer_id, amount, currency, description \\ "") do
     Stripe.Charge.create(%{customer: customer_id, amount: amount, currency: currency, description: description})
   end
+
+  def auth(customer_id, amount, currency, description \\ "") do
+    Stripe.Charge.create(%{customer: customer_id, amount: amount, currency: currency, description: description, capture: false})
+  end
+
+  def capture(charge_id) do
+    Stripe.Charge.capture(charge_id)
+  end
+
+  def refund(charge_id) do
+    Stripe.Refund.create(%{charge: charge_id})
+  end
 end
